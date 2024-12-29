2fcdd4feef0b90a10004b94239758df947e437a5
dbiddings444 <dbiddings444@gmail.com>
2024-12-29 11:10:51 -0500

    Back-end in progress

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+// const userController = require('../controllers/userController');
+const upload = require('../middleWare/upload');
+
+// router.post('/api/auth/signup', userController.signup);
+// router.post('/api/auth/login', userController.login);
+
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv'); 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const bodyParser = require('body-parser'); // Use require for body-parser
+
+dotenv.config({ path: '.env' });
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+// app.use('/uploads', express.static('uploads')); 
+// app.use(bodyParser.json({ limit: '30mb', extended: true }))
+// app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URL)
+  .then(() => console.log('Connected to Database'))
+  .catch((error) => console.error('Failed to connect to Database:', error));
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('The Server is up and running!!!');
+});
+
+// Use the authentication routes
+app.use('/', userRoutes);
+
+// Start the server
+app.listen(port, () => console.log(`Listening on port ${port}`));
